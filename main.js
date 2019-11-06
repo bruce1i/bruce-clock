5f883aff7bafdfe6e067cc1b2b4a10746590d4e1
bruce(lixun) <bruce.lixun@gmail.com>
2019-11-06 14:28:22 +0800

    v1.2.2

--- main.js
+++ main.js
@@ -8,14 +8,17 @@ const {
 } = require('electron')
 const path = require('path')
 
-const VERSION = 'v1.2.1'
+const VERSION = 'v1.2.2'
 const ONE_SECOND = 1000
 const LOOP_TIME = 10 * ONE_SECOND
+const INDEX_WIN_WIDTH = 78
+const INDEX_WIN_HEIGHT = 30
 let indexWin
 let tray
 let mainTimerId
 let checkingTime
 let hideTimerId
+let moveTimerId
 
 function createTray() {
     tray = new Tray(path.join(__dirname, '/assets/tray-icon.png'))
@@ -44,15 +47,17 @@ function showIndexWin() {
     if (indexWin) {
         indexWin.showInactive()
     } else {
-        const screenDisplay = screen.getPrimaryDisplay()
+        const {size} = screen.getPrimaryDisplay()
+        const x = size.width - INDEX_WIN_WIDTH
+        const y = 0
 
         indexWin = new BrowserWindow({
             // width: 800,
             // height: 600,
-            width: 78,
-            height: 30,
-            x: screenDisplay.size.width - 78,
-            y: 0,
+            width: INDEX_WIN_WIDTH,
+            height: INDEX_WIN_HEIGHT,
+            x: x,
+            y: y,
             frame: false,
             resizable: false,
             alwaysOnTop: true,
@@ -65,9 +70,18 @@ function showIndexWin() {
         // indexWin.webContents.openDevTools() // 关闭开发者工具窗口才可以窗体透明
 
         indexWin.on('minimize', () => {
+            /**
+             * 在触发最小化事件前，会先触发move事件，清空上一次的move定时器，防止卡死进程。
+             */
+            clearTimeout(moveTimerId)
+
             setTimeout(() => {
                 indexWin.show()
             }, 0)
+        })
+
+        indexWin.on("move", () => {
+            restoreIndexWinToDefaultPosition()
         })
 
         indexWin.loadFile('index.html')
@@ -91,6 +105,30 @@ function closeIndexWin() {
         indexWin.destroy()
         indexWin = null
     }
+}
+
+function restoreIndexWinToDefaultPosition() {
+    /**
+     * 当插拔外接显示器时，造成屏幕轻微闪烁，会对浮层位置产生一点点偏移；
+     * 在长时间睡眠后唤醒，也会造成位置偏移；
+     * 这用来修复当产生偏移时，还原默认位置。
+     *
+     * 注意：窗口收到最小化事件时，最先会触发move事件，所以延迟1s后进行检测，防止一直触发move事件卡死进程。
+     */
+    clearTimeout(moveTimerId)
+
+    moveTimerId = setTimeout(() => {
+        if (indexWin == null) return
+
+        const [currX, currY] = indexWin.getPosition()
+        const {size} = screen.getPrimaryDisplay()
+        const x = size.width - INDEX_WIN_WIDTH
+        const y = 0
+
+        if (currX !== x || currY !== y) {
+            indexWin.setPosition(x, y)
+        }
+    }, ONE_SECOND)
 }
 
 function generateCheckingTime() {
